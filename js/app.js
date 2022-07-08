0dcf1e187f56b9cb8310fe694462e6113b617636
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-08 20:22:26 +0100

    fetch answers data

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ const quiz_limit = document.querySelector('.quiz-limit');
 const quiz_difficulty = document.getElementsByName('difficulty');
 const start_quiz_btn = document.querySelector('.start-btn');
 
+const _question_category = document.getElementById('question-category');
+const _question = document.getElementById('question-title');
+const _options = document.getElementById('question-options');
+
 let category = '';
 let limit = '';
 let difficulty = '';
@@ -11,19 +15,57 @@ let difficulty = '';
 const questions = [];
 
 //API URL
-const API_URL = 'https://the-trivia-api.com/api/questions?';
+const API_URL = 'https://opentdb.com/api.php?';
+
+
+//load qustions from api
+async function loadQuestions() {
+    const settings = loadSettings();
+    // https://opentdb.com/api.php?amount=10&category=11&difficulty=easy
+    const result = await fetch(`${API_URL}amount=${settings.limit}&category=${settings.category}&difficulty=${settings.difficulty}`);
+    const data = await result.json();
+    // console.log(data.results[0]);
+    showQuestions(data.results[0]);
+}
+//function to show questions
+function showQuestions(data) {
+    const question = data.question;
+    const answers = data.incorrect_answers;
+    const correct_answer = data.correct_answer;
+    const question_type = data.type;
+    const question_category = data.category;
+
+    const options_data = [correct_answer, ...answers];
+    _question_category.innerHTML = question_category;
+    _question.innerHTML = question;
+    options_data.forEach(function (option) {
+        _options.innerHTML += `
+                <div class="option flex justify-start items-center my-2">
+                    <span class="ml-2">
+                        ${option}
+                    </span>
+                </div>`;
+    });
+}
+
+loadQuestions();
 
 //start timer
 setTimer(30);
 
-start_quiz_btn.addEventListener('click', (e) => {
-    setUpSettings();
-    if (category === 'Select Category' || limit === 'Select number of questions' || difficulty === '') {
-        alert('Please select all the fields');
-    } else {
-        window.location.href = 'quiz.html';
-    }
-});
+if(start_quiz_btn !== null) {
+    start_quiz_btn.addEventListener('click', (e) => {
+        setUpSettings();
+        if (category === 'Select Category' || limit === 'Select number of questions' || difficulty === '') {
+            alert('Please select all the fields');
+        } else {
+            saveSettings();
+            setTimeout(() => {
+                window.location.href = 'quiz.html';
+            }, 300);
+        }
+    });
+}
 
 
 //function to get the difficulty
@@ -37,8 +79,8 @@ function getDifficulty() {
 
 //function to set the settings
 function setUpSettings() {
-    if (quiz_category.value === 'Select Category') category = 'Science';
-    if (quiz_limit.value === 'Select number of questions') limit = '5';
+    category = quiz_category.value;
+    limit = quiz_limit.value;
     difficulty = getDifficulty();
 }
 
@@ -52,4 +94,32 @@ function setTimer(duration){
         //     // $('.result-wrapper').show();
         // }
     }, 1000);
+}
+
+//save settings to local storage
+function saveSettings() {
+    if (localStorage.getItem('category') === null) {
+        localStorage.setItem('category', category);
+    }
+    if (localStorage.getItem('limit') === null) {
+        localStorage.setItem('limit', limit);
+    }
+    if (localStorage.getItem('difficulty') === null) {
+        localStorage.setItem('difficulty', difficulty);
+    }
+}
+
+//load settings from local storage
+function loadSettings() {
+    let settings = {};
+    if (localStorage.getItem('category') !== null) {
+        settings.category = localStorage.getItem('category');
+    }
+    if (localStorage.getItem('limit') !== null) {
+        settings.limit = localStorage.getItem('limit');
+    }
+    if (localStorage.getItem('difficulty') !== null) {
+        settings.difficulty = localStorage.getItem('difficulty');
+    }
+    return settings;
 }
