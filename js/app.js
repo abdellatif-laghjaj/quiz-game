7002807c8f791808dccbbd3b4463601deaf544f5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-08 15:26:13 +0100

    get values from the form

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const quiz_category = document.querySelector('.quiz-category');
+const quiz_limit = document.querySelector('.quiz-limit');
+const quiz_difficulty = document.getElementsByName('difficulty');
+const start_quiz_btn = document.querySelector('.start-btn');
+
+let category = '';
+let limit = '';
+let difficulty = '';
+
+start_quiz_btn.addEventListener('click', (e) => {
+    setUpSettings();
+    if (category === 'Select Category' || limit === 'Select number of questions' || difficulty === '') {
+        alert('Please select all the fields');
+    } else {
+        console.log(category, limit, difficulty);
+    }
+});
+
+
+//function to get the difficulty
+function getDifficulty() {
+    for (let i = 0; i < quiz_difficulty.length; i++) {
+        if (quiz_difficulty[i].checked) {
+            return quiz_difficulty[i].value;
+        }
+    }
+}
+
+//function to set the settings
+function setUpSettings() {
+    category = quiz_category.value;
+    limit = quiz_limit.value;
+    difficulty = getDifficulty();
+}
