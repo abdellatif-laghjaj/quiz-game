d6785f45bfccd819cd6551821c15f3b96109d8df
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-08 18:22:49 +0100

    get answers in array

--- js/app.js
+++ js/app.js
@@ -3,10 +3,21 @@ const quiz_category = document.querySelector('.quiz-category');
 const quiz_limit = document.querySelector('.quiz-limit');
 const quiz_difficulty = document.getElementsByName('difficulty');
 const start_quiz_btn = document.querySelector('.start-btn');
+const answers_wrapper = document.querySelector('.answers-wrapper');
+const answers = document.querySelectorAll('label');
+const next_btn = document.querySelector('.next-btn');
 
 let category = '';
 let limit = '';
 let difficulty = '';
+
+const questions = [];
+
+//API URL
+const API_URL = 'https://the-trivia-api.com/api/questions?';
+
+//start timer
+setTimer(30);
 
 start_quiz_btn.addEventListener('click', (e) => {
     setUpSettings();
@@ -29,7 +40,58 @@ function getDifficulty() {
 
 //function to set the settings
 function setUpSettings() {
-    category = quiz_category.value;
-    limit = quiz_limit.value;
+    if (quiz_category.value === 'Select Category') category = 'Science';
+    if (quiz_limit.value === 'Select number of questions') limit = '5';
     difficulty = getDifficulty();
 }
+
+//timer countdown
+function setTimer(duration){
+    setInterval(function(){
+        $('.countdown span').css('--value', duration);
+        duration--;
+        if(duration < 0){
+            $('.quiz-wrapper').hide();
+            // $('.result-wrapper').show();
+        }
+    }, 1000);
+}
+
+
+//function to start the quiz
+function startQuiz() {
+    setTimer(60);
+    $('.setup-wrapper').hide();
+    $('.quiz-wrapper').show();
+    getQuestions();
+}
+
+//generate the questions array
+function generateQuestions(api, category, limit, difficulty) {
+    const questions = [];
+    const correctAnswer = [];
+    const wrongAnswers = [];
+
+    const url = `${api}categories=${category}&limit=${limit}&difficulty=${difficulty}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                wrongAnswers.push(data[i].incorrectAnswers.forEach(answer => {
+                    wrongAnswers.push(answer);
+                }));
+
+                correctAnswer.push(data[i].correctAnswer);
+                questions.push({
+                    category: data[i].category,
+                    difficulty: data[i].difficulty,
+                    question: data[i].question,
+                    answers: wrongAnswers.concat(correctAnswer),
+                    correctAnswer: correctAnswer
+                });
+            }
+        }).catch(error => console.log(error));
+    return questions;
+}
+
+console.log(generateQuestions(API_URL, 'Science', '5', 'easy'));
